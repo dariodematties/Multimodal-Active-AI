05e87e1bf3e6f90b86ea5c440495efa504443580
Dario Dematties <djdematties@gmail.com>
2020-11-15 12:33:54 +0000

    Optimizer related updates, checkpoint management, validation function, validation DALI pipes

--- Contrastive_Learning.py
+++ Contrastive_Learning.py
@@ -33,13 +33,13 @@ import math
 
 import numpy as np
 import torch
-import torch.optim as optim
 
 import torch.distributed as dist
 import torch.distributed.autograd as dist_autograd
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.distributed.optim import DistributedOptimizer
 from torch.distributed.rpc import RRef
+from pytorch_lightning.metrics import Accuracy
 
 from time import time
 
@@ -85,6 +85,8 @@ def parse():
                             metavar='N', help='print frequency (default: 10)')
         parser.add_argument('--resume', default='', type=str, metavar='PATH',
                             help='path to latest checkpoint (default: none)')
+        parser.add_argument('--optimizer', default='adam', type=str, metavar='OPTIM',
+                            help='optimizer for training the network (default: adam)')
         parser.add_argument('--dali_cpu', action='store_true',
                             help='Runs CPU based version of DALI pipeline.')
         parser.add_argument('--prof', default=-1, type=int,
@@ -113,6 +115,7 @@ def main():
                 args.distributed = int(os.environ['WORLD_SIZE']) > 1
         
 
+        best_prec1 = 0
         args.gpu = 0
         args.world_size = 1
         
@@ -160,31 +163,13 @@ def main():
                print('SimCLR_Module created from global rank number {}, local rank {}' .format(args.global_rank, args.local_rank))
         
         
-        # Optionally resume from a checkpoint
-        if args.resume:
-             # Use a local scope to avoid dangling references
-             def resume():
-                if os.path.isfile(args.resume):
-                    print("=> loading checkpoint '{}'" .format(args.resume))
-                    checkpoint = torch.load(args.resume, map_location = lambda storage, loc: storage.cuda(args.gpu))
-                    args.start_epoch = checkpoint['epoch']
-                    model.load_state_dict(checkpoint['state_dict'])
-                    optimizer.load_state_dict(checkpoint['optimizer'])
-                    print("=> loaded checkpoint '{}' (epoch {})"
-                                    .format(args.resume, checkpoint['epoch']))
-                else:
-                    print("=> no checkpoint found at '{}'" .format(args.resume))
-        
-             resume()
-
-
         path = args.data
         os.environ['DALI_EXTRA_PATH']=path
         test_data_root = os.environ['DALI_EXTRA_PATH']
         file_root = os.path.join(test_data_root, 'MSCOCO', 'cocoapi', 'images', 'train2014')
         annotations_file = os.path.join(test_data_root, 'MSCOCO', 'cocoapi', 'annotations', 'instances_train2014.json')
 
-        # This is pipe1, using this we bring image batches from MSCOCO dataset
+        # This is pipe1, using this we bring image batches from MSCOCO dataset for training
         # If there is more than one GPU, it will try to map each rank in a different GPU device
         if torch.cuda.device_count() > 1:
                 pipe1 = NDP.COCOReader(batch_size=args.batch_size,
@@ -208,11 +193,15 @@ def main():
         start = time()
         pipe1.build()
         total_time = time() - start
-        meta_data = pipe1.reader_meta()['__COCOReader_1']
+        pipe1_reader_name = '__COCOReader_1'
+        meta_data = pipe1.reader_meta()[pipe1_reader_name]
         if args.verbose:
                print('pipe1 built by global rank number {}, local rank number {} in {} seconds' .format(args.global_rank, args.local_rank, total_time))
                print('pipe1 dataset information from global rank number {}, local rank {} is {}' .format(args.global_rank, args.local_rank, meta_data))
-               print('pipe1 shard size for global rank number {}, local rank {} is {}' .format(args.global_rank, args.local_rank, compute_shard_size(pipe1)))
+               print('pipe1 shard size for global rank number {}, local rank {} is {}' .format(args.global_rank, args.local_rank, compute_shard_size(pipe1, pipe1_reader_name)))
+
+
+
 
 
         # This is pipe2, which is used to augment the batches brought by pipe1 utilizing foveated saccades
@@ -243,6 +232,56 @@ def main():
                print('pipe2 built by global rank number {}, local rank number {} in {} seconds' .format(args.global_rank, args.local_rank, total_time))
 
 
+
+
+
+
+
+
+
+        path = args.data
+        os.environ['DALI_EXTRA_PATH']=path
+        test_data_root = os.environ['DALI_EXTRA_PATH']
+        file_root = os.path.join(test_data_root, 'MSCOCO', 'cocoapi', 'images', 'val2014')
+        annotations_file = os.path.join(test_data_root, 'MSCOCO', 'cocoapi', 'annotations', 'instances_val2014.json')
+
+        # This is pipe3, using this we bring image batches from MSCOCO dataset for validation
+        # If there is more than one GPU, it will try to map each rank in a different GPU device
+        if torch.cuda.device_count() > 1:
+                pipe3 = NDP.COCOReader(batch_size=args.batch_size,
+                                       num_threads=args.workers,
+                                       device_id=args.local_rank,
+                                       file_root=file_root,
+                                       annotations_file=annotations_file,
+                                       shard_id=args.local_rank,
+                                       num_shards=args.world_size,
+                                       dali_cpu=args.dali_cpu)
+        # Otherwise, it will map all ranks on the same GPU device
+        else:
+                pipe3 = NDP.COCOReader(batch_size=args.batch_size,
+                                       num_threads=args.workers,
+                                       device_id=0,
+                                       file_root=file_root,
+                                       annotations_file=annotations_file,
+                                       shard_id=args.local_rank,
+                                       num_shards=args.world_size,
+                                       dali_cpu=args.dali_cpu)
+        start = time()
+        pipe3.build()
+        total_time = time() - start
+        pipe3_reader_name = '__COCOReader_22'
+        meta_data = pipe3.reader_meta()[pipe3_reader_name]
+        if args.verbose:
+               print('pipe3 built by global rank number {}, local rank number {} in {} seconds' .format(args.global_rank, args.local_rank, total_time))
+               print('pipe3 dataset information from global rank number {}, local rank {} is {}' .format(args.global_rank, args.local_rank, meta_data))
+               print('pipe3 shard size for global rank number {}, local rank {} is {}' .format(args.global_rank, args.local_rank, compute_shard_size(pipe3, pipe3_reader_name)))
+
+
+
+
+
+
+
         # For distributed training, wrap the model with torch.nn.parallel.DistributedDataParallel.
         if args.distributed:
                 if args.dali_cpu:
@@ -254,38 +293,87 @@ def main():
                         print('Since we are in a distributed setting the model is replicated here in global rank number {}, local rank {}' .format(args.global_rank, args.local_rank))
 
 
-        # Set optimizer and scale larning rate on global batch size
-        # args.lr = args.lr*float(args.batch_size*args.world_size)/256.
-        # optimizer = optim.SGD(model.parameters(), args.lr,
-                              # momentum=args.momentum,
-                              # weight_decay=args.weight_decay)
+        # Set optimizer
+        optimizer = Model_Util.get_optimizer(model, args)
+        if args.global_rank==0 and args.verbose:
+                print('Optimizer used for this run is {}'.format(args.optimizer))
 
-        optimizer = optim.Adam(model.parameters(), args.lr)
+        # Optionally resume from a checkpoint
+        if args.resume:
+             # Use a local scope to avoid dangling references
+             def resume():
+                if os.path.isfile(args.resume):
+                    print("=> loading checkpoint '{}'" .format(args.resume))
+                    checkpoint = torch.load(args.resume, map_location = lambda storage, loc: storage.cuda(args.gpu))
+                    start_epoch = checkpoint['epoch']
+                    best_prec1 = checkpoint['best_prec1']
+                    model.load_state_dict(checkpoint['state_dict'])
+                    optimizer.load_state_dict(checkpoint['optimizer'])
+                    print("=> loaded checkpoint '{}' (epoch {})"
+                                    .format(args.resume, checkpoint['epoch']))
+                    print("Model best precision saved was {}" .format(best_prec1))
+                    return start_epoch, best_prec1, model, optimizer
+                else:
+                    print("=> no checkpoint found at '{}'" .format(args.resume))
+        
+             args.start_epoch, best_prec1, model, optimizer = resume()
+
 
         total_time = AverageMeter()
         for epoch in range(args.start_epoch, args.epochs):
-                # train for one epoch
                 arguments = {'pipe1': pipe1,
                              'pipe2': pipe2,
+                             'pipe3': pipe3,
                              'images': images,
                              'model': model,
                              'optimizer': optimizer,
                              'warmup_epochs': args.warmup_epochs,
                              'train_epochs': args.epochs,
-                             'num_examples': compute_shard_size(pipe1),
+                             'num_examples': compute_shard_size(pipe1, pipe1_reader_name),
+                             'pipe1_reader_name': pipe1_reader_name,
+                             'pipe3_reader_name': pipe3_reader_name,
                              'epoch': epoch,
+                             'num_fixations': args.num_fixations,
                              'batch_size': args.batch_size,
                              'world_size': args.world_size,
                              'base_learning_rate': args.lr,
                              'learning_rate_scaling': args.lrs,
                              'temperature': args.temperature,
+                             'is_testing': args.test,
                              'device': device}
+
+                # train for one epoch
                 avg_train_time = train(arguments)
                 total_time.update(avg_train_time)
                 if args.test:
                         break
 
+                # evaluate on validation set
+                [prec1, prec5] = validate(arguments)
+
+                # remember the best prec@1 and save checkpoint
+                if args.global_rank == 0:
+                        print('From validation we have prec1 is {} while best_prec1 is {}'.format(prec1, best_prec1))
+                        is_best = prec1 > best_prec1
+                        best_prec1 = max(prec1, best_prec1)
+                        Model_Util.save_checkpoint({
+                                'epoch': epoch + 1,
+                                'state_dict': model.state_dict(),
+                                'best_prec1': best_prec1,
+                                'optimizer': optimizer.state_dict(),
+                        }, is_best)
+
+                        print('##Contrastive Top-1 {0}\n'
+                              '##Contrastive Top-5 {1}\n'
+                              '##Best Contrastive Top-1 saved {2}\n'
+                              '##Perf {3}'.format(
+                              prec1,
+                              prec5,
+                              best_prec1,
+                              args.total_batch_size / total_time.avg))
+
                 pipe1.reset()
+                pipe3.reset()
 
 
 
@@ -328,7 +416,7 @@ def train(arguments):
         arguments['model'].train()
         end = time()
 
-        shard_size = compute_shard_size(arguments['pipe1'])
+        shard_size = compute_shard_size(arguments['pipe1'], arguments['pipe1_reader_name'])
         i = 0
         while i*arguments['pipe1'].batch_size < shard_size:
                 # bring a new batch
@@ -363,7 +451,6 @@ def train(arguments):
                         outputs2 = arguments['model'](pipe2_output[0][5:])
 
                         # Compute Huber loss
-                        # loss = SimCLR.compute_loss(outputs1.data, outputs2, arguments['temperature'])
                         loss, logits, labels = Objective.contrastive_loss(hidden1=outputs1.data,
                                                                           hidden2=outputs2,
                                                                           temperature=arguments['temperature'],
@@ -381,6 +468,10 @@ def train(arguments):
                         arguments['optimizer'].step()
                         outputs1=outputs2
 
+
+                if arguments['is_testing']:
+                        if i > 10:
+                                break
 
                 if i%args.print_freq == 0:
                         # Every print_freq iterations, check the loss and speed.
@@ -424,6 +515,115 @@ def train(arguments):
 
 
 
+def validate(arguments):
+        batch_time = AverageMeter()
+        top1_prec = AverageMeter()
+        top5_prec = AverageMeter()
+
+        # switch to evaluate mode
+        arguments['model'].eval()
+
+        end = time()
+
+        accuracy = Accuracy()
+
+        shard_size = compute_shard_size(arguments['pipe3'], arguments['pipe3_reader_name'])
+        i = 0
+        while i*arguments['pipe3'].batch_size < shard_size:
+                # bring a new batch
+                pipe3_output = arguments['pipe3'].run()
+                images_gpu = pipe3_output[0]
+
+                bboxes_cpu = pipe3_output[1]
+                labels_cpu = pipe3_output[2]
+
+                val_loader_len = int(math.ceil(shard_size / arguments['pipe3'].batch_size))
+
+                with torch.no_grad():
+                        # set images for pipe2
+                        arguments['images'].data = images_gpu
+
+                        # set fixation parameters for pipe2
+                        NDP.fixation_pos_x = torch.rand((args.batch_size,1))
+                        NDP.fixation_pos_y = torch.rand((args.batch_size,1))
+                        NDP.fixation_angle = (torch.rand((args.batch_size,1))-0.5)*60
+
+                        # make the first saccade
+                        pipe2_output = NDP.pytorch_wrapper([arguments['pipe2']])
+                        outputs1 = arguments['model'](pipe2_output[0][5:])
+
+                        # set the second round of fixation parameters for pipe2
+                        NDP.fixation_pos_x = torch.rand((args.batch_size,1))
+                        NDP.fixation_pos_y = torch.rand((args.batch_size,1))
+                        NDP.fixation_angle = (torch.rand((args.batch_size,1))-0.5)*60
+                        
+                        # make the second saccade
+                        pipe2_output = NDP.pytorch_wrapper([arguments['pipe2']])
+                        outputs2 = arguments['model'](pipe2_output[0][5:])
+
+                        # Compute Huber loss
+                        loss, logits, labels = Objective.contrastive_loss(hidden1=outputs1.data,
+                                                                          hidden2=outputs2,
+                                                                          temperature=arguments['temperature'],
+                                                                          local_rank=args.local_rank,
+                                                                          world_size=args.world_size,
+                                                                          device=arguments['device'])
+
+                        # contrastive_top_1_accuracy = accuracy(torch.argmax(logits, dim=1).to('cpu'), torch.argmax(labels, dim=1).to('cpu'))
+                        contrastive_top_1_accuracy = Model_Util.top_k_accuracy(logits, labels, 1)
+                        contrastive_top_5_accuracy = Model_Util.top_k_accuracy(logits, labels, 5)
+
+
+                if args.distributed:
+                        reduced_top1 = reduce_tensor(contrastive_top_1_accuracy.data)
+                        reduced_top5 = reduce_tensor(contrastive_top_5_accuracy.data)
+                else:
+                        reduced_top1 = contrastive_top_1_accuracy.data
+                        reduced_top5 = contrastive_top_5_accuracy.data
+
+
+                top1_prec.update(to_python_float(reduced_top1), pipe2_output[0][5].size(0))
+                top5_prec.update(to_python_float(reduced_top5), pipe2_output[0][5].size(0))
+
+                # measure elapsed time
+                batch_time.update((time() - end)/args.print_freq)
+                end = time()
+
+                if args.local_rank == 0 and i % args.print_freq == 0:
+                        print('Test: [{0}/{1}]\t'
+                              'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                              'Speed {2:.3f} ({3:.3f})\t'
+                              'Cont. Top-1 Acc {top1.val:.4f} ({top1.avg:.4f})\t'
+                              'Cont. Top-5 Acc {top5.val:.4f} ({top5.avg:.4f})'.format(
+                              i, val_loader_len,
+                              args.world_size*args.batch_size/batch_time.val,
+                              args.world_size*args.batch_size/batch_time.avg,
+                              batch_time=batch_time,
+                              top1=top1_prec,
+                              top5=top5_prec))
+                
+                i += 1
+
+        return [top1_prec.avg, top5_prec.avg]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -457,8 +657,8 @@ class AverageMeter(object):
 
 
 
-def compute_shard_size(pipe):
-        meta_data = pipe.reader_meta()['__COCOReader_1']
+def compute_shard_size(pipe, reader_name):
+        meta_data = pipe.reader_meta()[reader_name]
 
         if meta_data['pad_last_batch'] == 1:
                 shards_beg = np.floor(meta_data['shard_id'] * meta_data['epoch_size_padded'] / meta_data['number_of_shards']).astype(np.int)
--- SimCLR/Model_Util.py
+++ SimCLR/Model_Util.py
@@ -1,14 +1,17 @@
 import torch
+import shutil
 import math
+import torch.optim as optim
+from apex.parallel import LARC
 
 
 def learning_rate_schedule(arguments):
         """Build learning rate schedule."""
         optimizer_params = arguments['optimizer'].state[arguments['optimizer'].param_groups[0]["params"][-1]]
         if 'step' in optimizer_params:
-                global_step = optimizer_params['step']
+                global_step = optimizer_params['step'] // arguments['num_fixations']
         else:
-                global_step = 1
+                global_step = 1 // arguments['num_fixations']
 
         # warmup_steps = warmup_epochs * num_examples / batch_size
         warmup_steps = int(round(arguments['warmup_epochs'] * arguments['num_examples'] // arguments['batch_size']))
@@ -55,3 +58,51 @@ def _get_train_steps(num_examples, train_epochs, train_batch_size):
         """Determine the number of training steps."""
         return num_examples * train_epochs // train_batch_size + 1
 
+
+
+
+
+
+
+def get_optimizer(model, args):
+        """Returns an optimizer."""
+        if args.optimizer == 'sgd':
+                optimizer = optim.SGD(model.parameters(),
+                                      args.lr,
+                                      momentum=args.momentum,
+                                      weight_decay=args.weight_decay)
+
+        elif args.optimizer == 'adam':
+                optimizer = optim.Adam(model.parameters(),
+                                       args.lr)
+
+        elif args.optimizer == 'lars':
+                optimizer = optim.Adam(model.parameters(),
+                                       args.lr)
+                optimizer = LARC.LARC(optimizer)
+
+        else:
+                raise ValueError('Unknown optimizer {}'.format(args.optimizer))
+
+        return optimizer
+
+
+
+
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+        torch.save(state, filename)
+        if is_best:
+                print('Saving a new best model with precesion {}'.format(state['best_prec1']))
+                shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+
+
+def top_k_accuracy(preds, target, k):
+        a=torch.transpose(torch.topk(preds,k=k,dim=1)[1],0,1)
+        b=torch.argmax(target,dim=1)
+        c=a==b
+        d=torch.any(c,dim=0)
+        return torch.sum(d)/(d.shape[0] + 0.0)
