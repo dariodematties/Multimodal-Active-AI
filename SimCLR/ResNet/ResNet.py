4381b19df97e209257ddb5d8925998d35146b298
Dario Dematties <djdematties@gmail.com>
2021-03-01 15:45:43 +0000

    Incorporation of sync batch normalization

--- SimCLR/ResNet/ResNet.py
+++ SimCLR/ResNet/ResNet.py
@@ -22,17 +22,20 @@ class BasicBlock(nn.Module):
 
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         # self.bn1 = nn.GroupNorm(planes//2, planes)
-        self.bn1 = nn.BatchNorm2d(planes)
+        # self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.SyncBatchNorm(planes)
 
         self.conv2 = nn.Conv2d(planes,    planes, kernel_size=3, stride=1,      padding=1, bias=False)
         # self.bn2 = nn.GroupNorm(planes//2, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
+        # self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.SyncBatchNorm(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                     nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                    nn.BatchNorm2d(self.expansion*planes)
+                    # nn.BatchNorm2d(self.expansion*planes)
+                    nn.SyncBatchNorm(self.expansion*planes)
                     )
 
 
@@ -52,22 +55,26 @@ class Bottleneck(nn.Module):
         super(Bottleneck, self).__init__()
 
         self.conv1 = nn.Conv2d(in_planes, planes,                kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
         # self.bn1 = nn.GroupNorm(planes//2, planes)
+        # self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = nn.SyncBatchNorm(planes)
 
         self.conv2 = nn.Conv2d(planes,    planes,                kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
         # self.bn2 = nn.GroupNorm(planes//2, planes)
+        # self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = nn.SyncBatchNorm(planes)
 
         self.conv3 = nn.Conv2d(planes,    self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
         # self.bn3 = nn.GroupNorm(self.expansion*planes//2, self.expansion*planes)
+        # self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.bn3 = nn.SyncBatchNorm(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                     nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                    nn.BatchNorm2d(self.expansion*planes)
+                    # nn.BatchNorm2d(self.expansion*planes)
+                    nn.SyncBatchNorm(self.expansion*planes)
                     )
 
 
